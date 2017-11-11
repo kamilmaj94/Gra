c2302b6dbf65041309054bb003ce6a7e4c6c932a
kamilmaj94 <kamilmaj94@hotmail.com>
2017-11-11 23:23:01 +0100

    Move Texture to separate file

---
+++ Gra/Globals.cpp
@@ -0,0 +1,8 @@
+#include "PCH.hpp"
+#include "Globals.hpp"
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
---
+++ Gra/Globals.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+//The window we'll be rendering to
+extern SDL_Window* gWindow;
+
+//The window renderer
+extern SDL_Renderer* gRenderer;
