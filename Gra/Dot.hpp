ee052b4c4beb286abeb60fa536a475acdc96d487
kamilmaj94 <kamilmaj94@hotmail.com>
2017-11-14 22:26:27 +0100

    First steps to implement physics in the game
    Basic stuff works.
    
    Change-Id: Iaba2f34fee93e961179648371ac9f508edb32480

--- Gra/Dot.hpp
+++ Gra/Dot.hpp
@@ -9,21 +9,51 @@ class Dot
 public:
     Dot();
 
-    bool Init();
+    bool Init(b2World& world);
     void HandleEvent( SDL_Event& e );
-    void Move();
+    //void Move(SDL_Event& e);
     void Render() const;
+
+    FORCEINLINE void SetPosition(b2Vec2 position)
+    {
+        mBody->SetTransform(position, angle);
+    }
+
+    FORCEINLINE void SetVelocity(b2Vec2 velocity)
+    {
+        mBody->SetLinearVelocity(velocity);
+    }
+
+    FORCEINLINE void SetAngle(float32 angle)
+    {
+        this->angle = angle;
+    }
+
+    FORCEINLINE b2Body *GetBody() const
+    {
+        return mBody;
+    }
+
+    FORCEINLINE b2Vec2 GetPosition() const
+    {
+        return position;
+    }
+
+    FORCEINLINE b2Vec2 GetVelocity() const
+    {
+        return velocity;
+    }
 
 private:
     static const int DOT_WIDTH = 20;
     static const int DOT_HEIGHT = 20;
-    static const int DOT_VEL = 10;
+    static const int DOT_VEL = 1000000;
 
-    int mPosX;
-    int mPosY;
-    int mVelX;
-    int mVelY;
+    b2Vec2 position;
+    b2Vec2 velocity;
+    float32 angle;
 
-    // TODO temporary - should probably capture a pointer to texture
     Texture mDotTexture;
+
+    b2Body* mBody;
 };
